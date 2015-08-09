7f4400fe8f8b5b7bf73a183e5af25f1bf75d99ae
Jeremy Marc <jeremy.marc@icloud.com>
2015-08-09 00:21:58 -0500

    add meal model

---
+++ app/models/meal.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: meals
+#
+#  calories    :integer
+#  created_at  :datetime         not null
+#  description :text
+#  eaten_at    :datetime
+#  id          :integer          not null, primary key
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
+class Meal < ActiveRecord::Base
+  belongs_to :user
+
+  scope :from_user, ->(user) { where(user: user) }
+  scope :from_date, ->(datetime) { where("eaten_at < ?", datetime) }
+  scope :to_date, ->(datetime) { where("eaten_at > ?", datetime) }
+end
