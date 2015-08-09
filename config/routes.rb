cbcd70f796d6b72d28f34d754dce19614a4937af
Jeremy Marc <jeremy.marc@icloud.com>
2015-08-09 00:05:04 -0500

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  mount Sidekiq::Web => "/sidekiq" # monitoring console
+  devise_for :users
+  # mount Sidekiq::Web => "/sidekiq" # monitoring console
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
