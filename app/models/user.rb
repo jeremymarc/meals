1492b7a2e74a6fb81e709103c65e4828b41ddd2d
Jeremy Marc <jeremy.marc@icloud.com>
2015-08-09 00:22:23 -0500

    add relation between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  created_at             :datetime         not null
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
+#  daily_calorie          :integer
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  id                     :integer          not null, primary key
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  updated_at             :datetime         not null
+#
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :meals, dependent: :destroy
 end
