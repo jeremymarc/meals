3491a40e893ad20b22546244b4b982c1e6e38123
Jeremy Marc <jeremy.marc@icloud.com>
2015-08-11 22:20:14 -0500

    remove files not used (sidekiq config, view helpers, empty folders)

--- app/helpers/api/meals_helper.rb
+++
@@ -1,2 +0,0 @@
-module Api::MealsHelper
-end
--- config/deploy/staging.rb
+++
@@ -1,6 +0,0 @@
-set :branch, ENV.fetch("CAPISTRANO_BRANCH", "development")
-set :mb_sidekiq_concurrency, 1
-
-server "staging.calorie_toptal.com",
-       :user => "deployer",
-       :roles => %w(app backup cron db redis sidekiq web)
