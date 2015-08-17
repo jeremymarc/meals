8031823af0c1e8c4058a700e49002efcc0ecec95
Jeremy Marc <jeremy.marc@icloud.com>
2015-08-17 11:25:50 -0500

    add user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,13 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  role                   :integer
 #  sign_in_count          :integer          default(0), not null
 #  updated_at             :datetime         not null
 #
+
+# encoding: utf-8
+require 'digest/md5'
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
@@ -25,4 +29,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :meals, dependent: :destroy
+  validates :email, presence: true
+  enum role: [:user, :admin]
+
+  def profile_image
+    'http://www.gravatar.com/avatar/' + Digest::MD5.hexdigest(email) + 'fs=50'
+  end
 end
