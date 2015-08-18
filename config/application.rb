992de507cf7b0bb96a7ac49d8d9107c3dd0f89c7
Jeremy Marc <jeremy.marc@icloud.com>
2015-08-18 12:44:13 -0500

    allows CORS for :delete

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Toptal
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
       end
     end
   end
